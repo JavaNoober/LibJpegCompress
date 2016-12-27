735ae9e4e90a6cd955a37fd859ab27687b73d658
xiaoqi <812817883@qq.com>
2016-12-27 11:27:40 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaoqi/libjpegcompress/MyApplication.java
@@ -0,0 +1,14 @@
+package com.xiaoqi.libjpegcompress;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+	public static boolean isSuppoertLibjpeg = false;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		isSuppoertLibjpeg = ImageUtils.isSupportLibJpeg();
+	}
+}
